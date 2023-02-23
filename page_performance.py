9e718f5353d6f5c8f08e185232fdbd21e8f4f269
Carlos Fuentes <104155059+cjfuentes-datascience@users.noreply.github.com>
2023-02-22 23:57:24 -0500

    Update page_performance.py

--- page_performance.py
+++ page_performance.py
@@ -2,19 +2,19 @@ import requests
 import json
 import streamlit as st
 
-
+# Define a function to analyze the website performance
 def analyze(url):
-    # Set up the API request URL
+    # Set up the API request URL using the provided website URL
     api_url = "https://www.googleapis.com/pagespeedonline/v5/runPagespeed?url={}&strategy=mobile".format(url)
 
-    # Send the API request and get the response as JSON data
+    # Send the API request to the Google PageSpeed API and get the response as JSON data
     response = requests.get(api_url)
     data = json.loads(response.text)
 
-    # Extract relevant performance metrics from the JSON data
+    # Extract relevant performance metrics from the JSON data received from the API
     lighthouse_data = data['lighthouseResult']['audits']
 
-    # Return the performance metrics
+    # Return the performance metrics as a dictionary
     return {
         "First Contentful Paint": lighthouse_data['first-contentful-paint']['displayValue'],
         "Speed Index": lighthouse_data['speed-index']['displayValue'],
@@ -25,16 +25,20 @@ def analyze(url):
         #"First Input Delay": lighthouse_data['first-input-delay']['displayValue']
     }
 
+# Set the title for the Streamlit web app
 st.title("Website Performance Analyzer :horse_racing:")
 
+# Get the URL of the website to analyze from the user
 url = st.text_input('Enter the URL of the website to analyze:')
 st.write('---')
 
+# If the URL is provided, analyze the website performance and display the results
 if url:
     results = analyze(url)
     st.subheader('Website Performance Metrics:')
     for key, value in results.items():
         st.write(f'{key}: {value}')
 else:
+    # If the URL is not provided, don't display any results
     #st.write('Enter a URL to analyze website performance')
     pass
