9d3146ede4c8307a25cccc9c11477c757d8c959d
Carlos Fuentes <104155059+cjfuentes-datascience@users.noreply.github.com>
2023-02-23 00:10:15 -0500

    Update page_performance.py

--- page_performance.py
+++ page_performance.py
@@ -1,9 +1,36 @@
 import requests
 import json
 import streamlit as st
+import random
 
 # Set the title of the browser tab
 st.set_page_config(page_title="Website Performance Analyzer :horse_racing:")
+
+quotes = [
+    "I cannot wait to win this match, friend!",
+    "The championship is ours!",
+    "Time to win, and stay alive!",
+    "Good luck. Have fun. Don't die!",
+    "I accept the challenge!",
+    "Let's have fun, friend!",
+    "This is going to be fun.",
+    "We will defeat all of our enemies!",
+    "Ready to go.",
+    "Powered up and ready to go.",
+    "I am ready. I hope you are too!",
+    "This is my favorite part!",
+    "Ready and excited.",
+    "I was created for this! ..I think.",
+    "I think we're going to do great, friend!",
+    "Excellent. Time to destroy more opponents.",
+    "You and me, friend!",
+    "Come on friend, time to win!",
+    "Grapple: locked and loaded.",
+    "I just polished my grapple.",
+    "A match a day keeps the doctor away! Just kidding, I'm made of metal.",
+    "Let's make some new friends, and then destroy them.",
+    "Today smells like victory! I love pretending to smell. Sniff sniff!"
+    ]
 
 # Define a function to analyze the website performance
 def analyze(url):
@@ -43,7 +70,7 @@ if url:
     for key, value in results.items():
         st.write(f'{key}: {value}')
     st.write('---')
-    st.text("I was created for this! ..I think.")
+    st.write(random.choice(quotes))
 else:
     # If the URL is not provided, don't display any results
     #st.write('Enter a URL to analyze website performance')
